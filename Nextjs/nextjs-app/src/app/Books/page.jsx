500970889591dec2b98ac03807c178724a7a5e0e
raihankabir1952 <raihankabir1952@gmail.com>
2026-09-17 23:15:38 +0600

    cache

--- Nextjs/nextjs-app/src/app/Books/page.jsx
+++ Nextjs/nextjs-app/src/app/Books/page.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { cache } from 'react';
 import BooksCard from '../components/BooksCard';
 
 const getBooks = async () => {
-    const data = await fetch('http://localhost:3001/books')
+    const data = await fetch('http://localhost:3001/books',
+        {next: {revalidate: 20}})
+
+        /*next: {revalidate: 10} means that the data will be revalidated every 10 seconds. 
+        This is useful for data that changes frequently, such as stock prices or news articles.*/
+
+    // const data = await fetch('http://localhost:3001/books' , {cache :'force-cache'})
     return data.json()
 }
 
